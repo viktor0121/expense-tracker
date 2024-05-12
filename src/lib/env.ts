baca51c4c2ab19bb3479fd90565811f4fec88886
viktor0121 <nikitinviktor0121@gmail.com>
2024-05-12 14:33:58 +0530

    Environment variable example file and ts file created

---
+++ src/lib/env.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+const EnvSchema = z.object({
+  awProjectUrl: z.string(),
+  awProjectId: z.string(),
+  awDatabaseId: z.string(),
+  awExpenseCollectionId: z.string(),
+  awIncomeCollectionId: z.string(),
+});
+
+const env = {
+  awProjectUrl: process.env.NEXT_PUBLIC_AW_PROJECT_URL as string,
+  awProjectId: process.env.NEXT_PUBLIC_AW_PROJECT_ID as string,
+  awDatabaseId: process.env.NEXT_PUBLIC_AW_DATABASE_ID as string,
+  awExpenseCollectionId: process.env.NEXT_PUBLIC_AW_EXPENSE_COLLECTION_ID as string,
+  awIncomeCollectionId: process.env.NEXT_PUBLIC_AW_INCOME_COLLECTION_ID as string,
+};
+
+try {
+  EnvSchema.parse(env);
+} catch (error: any) {
+  throw new Error(`Environment variable validation error: ${error.message}`);
+}
+
+// console.log(env);
+export default env;
