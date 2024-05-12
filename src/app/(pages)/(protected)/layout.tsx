564c85f197ad020153d3e89f8f8ede32015044d9
viktor0121 <nikitinviktor0121@gmail.com>
2024-05-12 23:06:18 +0530

    add protected routes

---
+++ src/app/(pages)/(protected)/layout.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+import useAuth from "@/context/auth/useAuth";
+
+export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const { authStatus } = useAuth();
+
+  if (!authStatus) {
+    router.replace("/signin");
+    return <></>;
+  }
+
+  return children;
+}
--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import authService from "@/lib/appwrite/auth";
 import { AuthProvider } from "@/context/auth/context";
 
 export default function AppLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   const [authStatus, setAuthStatus] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    // Check if user is logged in
+    authService
+      .isLoggedIn()
+      .then(setAuthStatus)
+      .finally(() => setIsLoading(false));
+  }, []);
 
   return (
     <AuthProvider value={{ authStatus, setAuthStatus }}>
