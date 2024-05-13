5f27ce7a94e92f17de7f10b8c2e91dc244e092a6
viktor0121 <nikitinviktor0121@gmail.com>
2024-05-13 21:17:54 +0530

    add react-hooks-form to signin signup tabs in auth page

--- src/components/signin-card.tsx
+++ src/components/signin-card.tsx
@@ -1,10 +1,43 @@
 import Link from "next/link";
-import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 export default function SignInCard() {
+  const formSchema = z.object({
+    email: z
+      .string()
+      .email("Invalid email address")
+      .min(1, "Email is required")
+      .max(100, "Email must be at most 100 characters"),
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters long")
+      .regex(/[a-z]/, "Password must contain a lowercase letter")
+      .regex(/[A-Z]/, "Password must contain an uppercase letter")
+      .regex(/\d/, "Password must contain a number"),
+  });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const submit = form.handleSubmit(async () => {});
+
   return (
     <Card>
       <CardHeader>
@@ -12,25 +45,49 @@ export default function SignInCard() {
         <CardDescription>Enter your email below to login to your account</CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" placeholder="m@example.com" required type="email" />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" required type="password" />
-            <Link className="ml-auto inline-block text-xs underline" href="#">
+        <Form {...form}>
+          <form onSubmit={submit} className="grid gap-3">
+            {/*Email Input*/}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" placeholder="me@example.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/*Password Input*/}
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/*Forgot Password */}
               Forgot your password?
             </Link>
-          </div>
-          <Button className="w-full" type="submit">
-            Login
-          </Button>
-          <Button className="w-full" variant="outline">
-            Login with Google
-          </Button>
-        </div>
+
+            {/*Submit Button*/}
+            <Button type="submit" className="mt-2 w-full">
+              Sign in to account
+            </Button>
+          </form>
+        </Form>
+
         <div className="mt-4 text-center text-sm">
           Don&apos;t have an account?{" "}
           <Link className="underline" href="#">
--- src/components/signup-card.tsx
+++ src/components/signup-card.tsx
@@ -1,10 +1,58 @@
 import Link from "next/link";
 import { Label } from "@/components/ui/label";
+"use client";
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 export default function SignUpCard() {
+  const formSchema = z.object({
+    firstName: z
+      .string()
+      .trim()
+      .min(1, "First Name is required")
+      .max(100, "First Name must be at most 100 characters"),
+    lastName: z
+      .string()
+      .trim()
+      .min(1, "Last Name is required")
+      .max(100, "Last Name must be at most 100 characters"),
+    email: z
+      .string()
+      .email("Invalid email address")
+      .min(1, "Email is required")
+      .max(100, "Email must be at most 100 characters"),
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters long")
+      .regex(/[a-z]/, "Password must contain a lowercase letter")
+      .regex(/[A-Z]/, "Password must contain an uppercase letter")
+      .regex(/\d/, "Password must contain a number"),
+  });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const submit = form.handleSubmit(async () => {});
+
   return (
     <Card>
       <CardHeader>
@@ -12,32 +60,75 @@ export default function SignUpCard() {
         <CardDescription>Enter your information to create an account</CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="grid gap-4">
-          <div className="grid grid-cols-2 gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="first-name">First name</Label>
-              <Input id="first-name" placeholder="Max" required />
+        <Form {...form}>
+          <form onSubmit={submit} className="grid gap-3">
+            {/*Name Input*/}
+            <div className="grid grid-cols-2 gap-3">
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>First Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Max" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Last Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Robinson" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
             </div>
-            <div className="grid gap-2">
-              <Label htmlFor="last-name">Last name</Label>
-              <Input id="last-name" placeholder="Robinson" required />
-            </div>
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" placeholder="m@example.com" required />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" />
-          </div>
-          <Button type="submit" className="w-full">
-            Create an account
-          </Button>
-          <Button variant="outline" className="w-full">
-            Sign up with GitHub
-          </Button>
-        </div>
+
+            {/*Email Input*/}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" placeholder="me@example.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/*Password Input*/}
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/*Submit Button*/}
+            <Button type="submit" className="mt-2 w-full">
+              Create an account
+            </Button>
+          </form>
+        </Form>
         <div className="mt-4 text-center text-sm">
           Already have an account?{" "}
           <Link href="#" className="underline">
