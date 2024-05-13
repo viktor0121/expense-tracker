77ffd0d2d731823452dbb079fb4aea2aff04e621
viktor0121 <nikitinviktor0121@gmail.com>
2024-05-13 21:05:52 +0530

    add feature to auto select auth tab based on searchParam

--- src/app/(pages)/auth/page.tsx
+++ src/app/(pages)/auth/page.tsx
@@ -1,20 +1,49 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import useReplaceSearchParam from "@/hooks/useReplaceSearchParam";
 import SignInCard from "@/components/signin-card";
 import SignUpCard from "@/components/signup-card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { EAuthTabs } from "@/lib/enums";
 
 export default function AuthPage() {
-  // Set default tab to search param specified / login
   const searchParams = useSearchParams();
-  const defaultTab = searchParams.has("tab", EAuthTabs.Register)
-    ? EAuthTabs.Register
-    : EAuthTabs.Login;
+  const replaceSearchParam = useReplaceSearchParam();
+  const [tab, setTab] = useState<EAuthTabs>();
 
+  /**
+   * handleTabOnChange function is responsible for handling the tab changes.
+   * It updates the URL search param "tab" and the local state "tab".
+   * @param {string} newTab - The new tab value.
+   */
+  const handleTabOnChange = (newTab: string) => {
+    replaceSearchParam("tab", newTab);
+    setTab(newTab as EAuthTabs);
+  };
+
+  /**
+   * useEffect hook is responsible for setting the initial tab value based on the URL search param "tab".
+   */
+  useEffect(() => {
+    const tabParam = searchParams.get("tab");
+    if (!tabParam) replaceSearchParam("tab", EAuthTabs.Login);
+    else if (tabParam === EAuthTabs.Register) setTab(EAuthTabs.Register);
+    else setTab(EAuthTabs.Login);
+  }, [searchParams]);
+
+  /**
+   * The rendered JSX of the AuthPage component.
+   * It includes the Tabs component with two tab triggers for Login and Register.
+   * It also includes the SignInCard and SignUpCard components as the content of the tabs.
+   */
   return (
-    <Tabs defaultValue={defaultTab} className="max-w-[500px] px-4 sm:px-6 mt-16 mx-auto">
+    <Tabs
+      value={tab}
+      onValueChange={handleTabOnChange}
+      className="max-w-[500px] px-4 sm:px-6 mt-16 mx-auto"
+    >
       <TabsList className="grid w-full grid-cols-2">
         <TabsTrigger value={EAuthTabs.Login}>Login</TabsTrigger>
         <TabsTrigger value={EAuthTabs.Register}>Register</TabsTrigger>
