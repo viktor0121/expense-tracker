57991b544eb6eb9e4ef78007b49505db885b7967
viktor0121 <nikitinviktor0121@gmail.com>
2024-05-22 03:25:51 +0530

    add form to add new saving-record

--- src/app/(pages)/(protected)/dashboard/components/saving-form.tsx
+++ src/app/(pages)/(protected)/dashboard/components/saving-form.tsx
@@ -1,3 +1,152 @@
+"use client";
+
+import { z } from "zod";
+import { format } from "date-fns";
+import { useForm } from "react-hook-form";
+import { CalendarIcon } from "lucide-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useToast } from "@/components/ui/use-toast";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import ButtonWithSpinner from "@/components/button-with-spinner";
+import { cn } from "@/lib/utils";
+
+const formSchema = z.object({
+  date: z.date({ required_error: "Date is required" }),
+  title: z
+    .string()
+    .trim()
+    .min(1, "Title is required")
+    .max(100, "First Name must be at most 100 characters"),
+  amount: z
+    .string()
+    .trim()
+    .min(1, "Amount must not be empty")
+    .refine((str) => /^[0-9.-]+$/.test(str), "Amount must contain only numbers")
+    .refine((str) => parseFloat(str) >= 0, "Amount must be a positive number")
+    .refine((str) => parseFloat(str) >= 0.01, "Amount must be at least 0.01")
+    .refine((str) => parseFloat(str) < 10_000_000, "Amount must be at most 10,00,00,000"),
+});
+
 export default function SavingForm() {
-  return <></>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      // TODO: Handle TS Error
+      // @ts-ignore
+      amount: "",
+    },
+  });
+
+  const { toast } = useToast();
+  const { isSubmitting, isValid } = form.formState;
+
+  const submit = form.handleSubmit(async ({ date, amount, title }) => {
+    console.log({ date, amount, title });
+
+    try {
+      // TODO: Handle saving form submission
+      toast({
+        title: "Success!",
+        description: "Your savings have been added successfully.",
+      });
+    } catch (error: any) {
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: error.message,
+      });
+    }
+  });
+
+  return (
+    <Form {...form}>
+      <form onSubmit={submit} className="grid gap-3">
+        {/*Name Input*/}
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter title" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="amount"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Amount</FormLabel>
+              <FormControl>
+                <Input type="number" placeholder="Enter amount" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="date"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Date</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground",
+                      )}
+                    >
+                      {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) => date > new Date() || date < new Date("1900-01-01")}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/*Submit Button*/}
+        <ButtonWithSpinner
+          isLoading={isSubmitting}
+          disabled={!isValid}
+          type="submit"
+          className="mt-2 w-full"
+          btnText="Add Record"
+        />
+      </form>
+    </Form>
+  );
 }
