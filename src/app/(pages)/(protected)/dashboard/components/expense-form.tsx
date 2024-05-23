3f01f5632ffc35c22ee507c43251ba008050b413
viktor0121 <nikitinviktor0121@gmail.com>
2024-05-23 23:56:53 +0530

    make date field in forms a reusable component

--- src/app/(pages)/(protected)/dashboard/components/expense-form.tsx
+++ src/app/(pages)/(protected)/dashboard/components/expense-form.tsx
@@ -29,6 +29,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import Link from "next/link";
+import FormDateField from "@/components/form-date-field";
 
 const formSchema = z.object({
   date: z.date({ required_error: "Date is required" }),
@@ -120,41 +121,7 @@ export default function ExpenseForm() {
           )}
         />
 
-        <FormField
-          control={form.control}
-          name="date"
-          render={({ field }) => (
-            <FormItem className="flex flex-col">
-              <FormLabel>Date</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant={"outline"}
-                      className={cn(
-                        "pl-3 text-left font-normal",
-                        !field.value && "text-muted-foreground",
-                      )}
-                    >
-                      {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
-                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
-                  <Calendar
-                    mode="single"
-                    selected={field.value}
-                    onSelect={field.onChange}
-                    disabled={(date) => date > new Date() || date < new Date("1900-01-01")}
-                    initialFocus
-                  />
-                </PopoverContent>
-              </Popover>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <FormDateField name="date" label="Date" placeholder="Pick a date" control={form.control} />
 
         <FormField
           control={form.control}
--- src/app/(pages)/(protected)/dashboard/components/saving-form.tsx
+++ src/app/(pages)/(protected)/dashboard/components/saving-form.tsx
@@ -21,6 +21,7 @@ import {
 } from "@/components/ui/form";
 import ButtonWithSpinner from "@/components/button-with-spinner";
 import { cn } from "@/lib/utils";
+import FormDateField from "@/components/form-date-field";
 
 const formSchema = z.object({
   date: z.date({ required_error: "Date is required" }),
@@ -103,41 +104,7 @@ export default function SavingForm() {
           )}
         />
 
-        <FormField
-          control={form.control}
-          name="date"
-          render={({ field }) => (
-            <FormItem className="flex flex-col">
-              <FormLabel>Date</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant={"outline"}
-                      className={cn(
-                        "pl-3 text-left font-normal",
-                        !field.value && "text-muted-foreground",
-                      )}
-                    >
-                      {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
-                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
-                  <Calendar
-                    mode="single"
-                    selected={field.value}
-                    onSelect={field.onChange}
-                    disabled={(date) => date > new Date() || date < new Date("1900-01-01")}
-                    initialFocus
-                  />
-                </PopoverContent>
-              </Popover>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <FormDateField name="date" label="Date" placeholder="Pick a date" control={form.control} />
 
         {/*Submit Button*/}
         <ButtonWithSpinner
