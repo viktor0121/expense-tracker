0bf689a0d837d6a1807e3c3ef9d88f4153479804
viktor0121 <nikitinviktor0121@gmail.com>
2024-05-27 23:49:37 +0530

    design bug fix

--- src/components/command-pallet.tsx
+++ src/components/command-pallet.tsx
@@ -82,8 +82,8 @@ export default function CommandPallet() {
 
         <Separator orientation="vertical" className="hidden sm:block mx-2" />
 
-        <kbd className="hidden sm:inline-flex pointer-events-none h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-          <span className="text-[1.1rem]">⌘ </span>K
+        <kbd className="hidden sm:inline-flex pointer-events-none h-5 select-none items-center justify-center gap-1 rounded border bg-muted px-1.5 font-mono text-xs font-medium text-muted-foreground opacity-100">
+          <span className="text-[0.6rem]">⌘</span>K
         </kbd>
       </Button>
 
