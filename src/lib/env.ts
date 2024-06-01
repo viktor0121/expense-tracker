52f462a3bc540ee459c2b97aaed544cee92527d2
viktor0121 <nikitinviktor0121@gmail.com>
2024-06-01 06:36:13 +0530

    env variable for storage added

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -7,6 +7,7 @@ const EnvSchema = z.object({
   awExpenseCollectionId: z.string(),
   awIncomeCollectionId: z.string(),
   awExpenseCategoryCollectionId: z.string(),
+  awProfilePhotoStorageId: z.string(),
 });
 
 const env = {
@@ -15,7 +16,8 @@ const env = {
   awDatabaseId: process.env.NEXT_PUBLIC_AW_DATABASE_ID as string,
   awExpenseCollectionId: process.env.NEXT_PUBLIC_AW_EXPENSE_COLLECTION_ID as string,
   awIncomeCollectionId: process.env.NEXT_PUBLIC_AW_INCOME_COLLECTION_ID as string,
-  awExpenseCategoryCollectionId: process.env.NEXT_PUBLIC_AW_EXPENSE_CATEGORY_COLLECTION_ID as string,
+  awExpenseCategoryCollectionId: process.env .NEXT_PUBLIC_AW_EXPENSE_CATEGORY_COLLECTION_ID as string,
+  awProfilePhotoStorageId: process.env.NEXT_PUBLIC_AW_PROFILE_PHOTO_STORAGE_ID as string,
 };
 
 try {
