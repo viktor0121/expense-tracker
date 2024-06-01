9a7b299fb63f7504414ef965a5aabb132039c185
viktor0121 <nikitinviktor0121@gmail.com>
2024-06-01 06:38:59 +0530

    add functions to create/delete profile photo & to generate profile photo url

---
+++ src/lib/appwrite/storage.ts
@@ -0,0 +1,62 @@
+import { Client, ID, ImageGravity, Storage } from "appwrite";
+import env from "@/lib/env";
+
+interface CreateProfilePhotoParams {
+  file: File;
+}
+
+interface GetProfilePhotoUrlParams {
+  photoId: string;
+}
+
+interface DeleteProfilePhotoParams {
+  photoId: string;
+}
+
+export class StorageService {
+  client = new Client();
+  storage;
+
+  constructor() {
+    this.client.setEndpoint(env.awProjectUrl).setProject(env.awProjectId);
+    this.storage = new Storage(this.client);
+  }
+
+  async createProfilePhoto({ file }: CreateProfilePhotoParams): Promise<string> {
+    try {
+      const photo = await this.storage.createFile(env.awProfilePhotoStorageId, ID.unique(), file);
+      return photo.$id;
+    } catch (error: any) {
+      console.error("Appwrite :: uploadProfilePhoto() :: ", error);
+      throw error;
+    }
+  }
+
+  getProfilePhotoUrl({ photoId }: GetProfilePhotoUrlParams): string {
+    try {
+      const url: URL = this.storage.getFilePreview(
+        env.awProfilePhotoStorageId,
+        photoId,
+        2000,
+        2000,
+        ImageGravity.Top,
+        100,
+      );
+      return url.toString();
+    } catch (error: any) {
+      console.error("Appwrite :: getProfilePhotoUrl() :: ", error);
+      throw error;
+    }
+  }
+
+  async deleteProfilePhoto({ photoId }: DeleteProfilePhotoParams): Promise<void> {
+    try {
+      await this.storage.deleteFile(env.awProfilePhotoStorageId, photoId);
+    } catch (error: any) {
+      console.error("Appwrite :: deleteProfilePhoto() :: ", error);
+      throw error;
+    }
+  }
+}
+
+export default new StorageService();
