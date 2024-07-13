aa3d413c3044e6c449fa56a043a96cba4d2690c1
viktor0121 <nikitinviktor0121@gmail.com>
2024-07-13 18:13:16 +0530

    Style changes in bg-grid-container component

--- src/components/bg-grid-container.tsx
+++ src/components/bg-grid-container.tsx
@@ -8,9 +8,9 @@ interface BgGridContainerProps {
 
 export function BgGridContainer({ children, className }: BgGridContainerProps) {
   return (
-    <div className={cn("dark:bg-grid-white/[0.1] bg-grid-black/[0.04] relative w-full", className)}>
+    <div className={cn("dark:bg-grid-white/[0.05] bg-grid-black/[0.1] relative w-full", className)}>
       {/* Radial gradient for the container to give a faded look */}
-      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       {children}
     </div>
   );
