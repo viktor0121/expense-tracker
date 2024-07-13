dc100a6259e334d948a093cc678d2c7cfd2e8cdb
viktor0121 <nikitinviktor0121@gmail.com>
2024-07-13 18:05:37 +0530

    Add new goal and goalList collection ID to env

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -7,6 +7,8 @@ const EnvSchema = z.object({
   awExpenseCollectionId: z.string(),
   awIncomeCollectionId: z.string(),
   awExpenseCategoryCollectionId: z.string(),
+  awGoalCollectionId: z.string(),
+  awGoalListCollectionId: z.string(),
   awProfilePhotoStorageId: z.string(),
 });
 
@@ -17,6 +19,8 @@ export const env = {
   awExpenseCollectionId: process.env.NEXT_PUBLIC_AW_EXPENSE_COLLECTION_ID as string,
   awIncomeCollectionId: process.env.NEXT_PUBLIC_AW_INCOME_COLLECTION_ID as string,
   awExpenseCategoryCollectionId: process.env.NEXT_PUBLIC_AW_EXPENSE_CATEGORY_COLLECTION_ID as string,
+  awGoalCollectionId: process.env.NEXT_PUBLIC_AW_GOAL_COLLECTION_ID as string,
+  awGoalListCollectionId: process.env.NEXT_PUBLIC_AW_GOAL_LIST_COLLECTION_ID as string,
   awProfilePhotoStorageId: process.env.NEXT_PUBLIC_AW_PROFILE_PHOTO_STORAGE_ID as string,
 };
 
