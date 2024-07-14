982acddd3f8202be91d6a8b8a59973e4875494a2
viktor0121 <nikitinviktor0121@gmail.com>
2024-07-14 20:50:46 +0530

    put the max width onto goal-bucket instead of page.tsx

--- src/app/(platform)/goal-buckets/layout.tsx
+++ src/app/(platform)/goal-buckets/layout.tsx
@@ -4,7 +4,7 @@ import { BgGridContainer } from "@/components/bg-grid-container";
 export default function GoalsLayout({ children }: { children: React.ReactNode }) {
   return (
     <BgGridContainer className="h-screen">
-      <div className="relative px-2 pt-4 sm:px-4 sm:pt-6">{children}</div>
+      <main className="relative mx-auto max-w-screen-xl px-2 pt-4 sm:px-4 sm:pt-6">{children}</main>
     </BgGridContainer>
   );
 }
--- src/app/(platform)/goal-buckets/page.tsx
+++ src/app/(platform)/goal-buckets/page.tsx
@@ -25,48 +25,46 @@ export default function GoalPage() {
 
   return (
     <>
-      <section className="mx-auto max-w-screen-xl">
-        <h2 className="mb-3 ml-2 mt-1 text-2xl font-semibold">Goal Buckets</h2>
+      <h2 className="mb-3 ml-2 mt-1 text-2xl font-semibold">Goal Buckets</h2>
 
-        {isGoalListsLoading ? (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            {[...Array(5)].map((_, index) => (
-              <div key={index} className="p-1.5">
-                <Skeleton className="flex h-60 w-full items-center justify-center rounded-md border">
-                  <Skeleton className="h-10 w-32 bg-opacity-25" />
-                </Skeleton>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            {goalLists.map((goalList, index) => (
-              <BgMotionCard key={index} index={index} activeIndex={activeIndex} setActiveIndex={setActiveIndex}>
-                <Link className="bg-red-400" href={`/goal-buckets/goals?bucketId=${goalList.$id}`}>
-                  <Card className="cursor-pointer">
-                    <CardContent className="flex min-h-60 items-center justify-center p-6">
-                      <CardTitle className="tracking-wide">{goalList.title}</CardTitle>
-                    </CardContent>
-                  </Card>
-                </Link>
-              </BgMotionCard>
-            ))}
-
-            <BgMotionCard index={-1} activeIndex={activeIndex} setActiveIndex={setActiveIndex}>
-              <Card onClick={createGoalBucketDialog.open} className="cursor-pointer">
-                <BgHoverDotsContainer>
+      {isGoalListsLoading ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {[...Array(5)].map((_, index) => (
+            <div key={index} className="p-1.5">
+              <Skeleton className="flex h-60 w-full items-center justify-center rounded-md border">
+                <Skeleton className="h-10 w-32 bg-opacity-25" />
+              </Skeleton>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {goalLists.map((goalList, index) => (
+            <BgMotionCard key={index} index={index} activeIndex={activeIndex} setActiveIndex={setActiveIndex}>
+              <Link className="bg-red-400" href={`/goal-buckets/goals?bucketId=${goalList.$id}`}>
+                <Card className="cursor-pointer">
                   <CardContent className="flex min-h-60 items-center justify-center p-6">
-                    <CardTitle className="flex items-center gap-1 tracking-wide">
-                      <PlusIcon className="size-8" />
-                      New
-                    </CardTitle>
+                    <CardTitle className="tracking-wide">{goalList.title}</CardTitle>
                   </CardContent>
-                </BgHoverDotsContainer>
-              </Card>
+                </Card>
+              </Link>
             </BgMotionCard>
-          </div>
-        )}
-      </section>
+          ))}
+
+          <BgMotionCard index={-1} activeIndex={activeIndex} setActiveIndex={setActiveIndex}>
+            <Card onClick={createGoalBucketDialog.open} className="cursor-pointer">
+              <BgHoverDotsContainer>
+                <CardContent className="flex min-h-60 items-center justify-center p-6">
+                  <CardTitle className="flex items-center gap-1 tracking-wide">
+                    <PlusIcon className="size-8" />
+                    New
+                  </CardTitle>
+                </CardContent>
+              </BgHoverDotsContainer>
+            </Card>
+          </BgMotionCard>
+        </div>
+      )}
     </>
   );
 }
