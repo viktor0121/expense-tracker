cf6892b2f01dd2792d9382eda30777c4149016c5
viktor0121 <nikitinviktor0121@gmail.com>
2024-07-17 00:39:52 +0530

    add indicator class name as prop to progress.tsx

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -4,17 +4,19 @@ import * as React from "react";
 import * as ProgressPrimitive from "@radix-ui/react-progress";
 import { cn } from "@/lib/utils";
 
-const Progress = React.forwardRef<
-  React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+interface IElement extends React.ElementRef<typeof ProgressPrimitive.Root> {}
+interface IProps extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  indicatorClassName: string;
+}
+
+const Progress = React.forwardRef<IElement, IProps>(({ className, indicatorClassName, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn("relative h-4 w-full overflow-hidden rounded-full bg-secondary", className)}
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
+      className={cn("h-full w-full flex-1 bg-primary transition-all", indicatorClassName)}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
