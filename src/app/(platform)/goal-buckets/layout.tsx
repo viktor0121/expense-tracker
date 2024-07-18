f8fb2703c70cf4e4e26e53dddcab2c0f04b0dedd
viktor0121 <nikitinviktor0121@gmail.com>
2024-07-18 21:54:36 +0530

    change max-width from `screen-xl` to `screen-2xl`

--- src/app/(platform)/goal-buckets/layout.tsx
+++ src/app/(platform)/goal-buckets/layout.tsx
@@ -4,7 +4,7 @@ import { BgGridContainer } from "@/components/bg-grid-container";
 export default function GoalsLayout({ children }: { children: React.ReactNode }) {
   return (
     <BgGridContainer className="h-screen">
-      <main className="relative mx-auto max-w-screen-xl px-2 pt-4 sm:px-4 sm:pt-6">{children}</main>
+      <main className="relative mx-auto max-w-screen-2xl px-2 pt-4 sm:px-4 sm:pt-6">{children}</main>
     </BgGridContainer>
   );
 }
