77131fed1a2fe88f025bfb0ba9b20597dd2abd8f
viktor0121 <nikitinviktor0121@gmail.com>
2024-07-20 18:54:40 +0530

    truncate title in update-bucket-dialog

--- src/components/overlays/update-goal-bucket-dialog.tsx
+++ src/components/overlays/update-goal-bucket-dialog.tsx
@@ -1,6 +1,7 @@
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { GoalBucketForm } from "@/components/forms/goal-bucket-form";
 import { useUpdateGoalBucketDialog } from "@/store/overlays/useUpdateGoalBucketDialog";
+import { truncateString } from "@/lib/utils";
 
 export function UpdateGoalBucketDialog() {
   const { isOpen, close, bucket } = useUpdateGoalBucketDialog();
@@ -11,8 +12,8 @@ export function UpdateGoalBucketDialog() {
     <Dialog open={isOpen} onOpenChange={close}>
       <DialogContent className="space-y-1 sm:max-w-lg">
         <DialogHeader>
-          <DialogTitle className="text-2xl">Update Bucket {bucket.title}</DialogTitle>
-          <DialogDescription>Update the goal bucket</DialogDescription>
+          <DialogTitle className="text-2xl">Update Bucket</DialogTitle>
+          <DialogDescription>Update the goal bucket {truncateString(bucket.title, 20)}</DialogDescription>
         </DialogHeader>
 
         <GoalBucketForm action="update" bucket={bucket} runAfterSubmit={close} />
