598af8ca6238e545d56e1cb666220813b8d91a0b
viktor0121 <nikitinviktor0121@gmail.com>
2024-08-03 21:19:18 +0530

    grid bg not completely showing on height greater than 100vh

--- src/app/(platform)/goal-buckets/layout.tsx
+++ src/app/(platform)/goal-buckets/layout.tsx
@@ -3,7 +3,7 @@ import { BgGridContainer } from "@/components/bg-grid-container";
 
 export default function GoalsLayout({ children }: { children: React.ReactNode }) {
   return (
-    <BgGridContainer className="h-screen">
+    <BgGridContainer className="min-h-screen">
       <main className="relative mx-auto max-w-screen-2xl px-2 pt-4 sm:px-4 sm:pt-6">{children}</main>
     </BgGridContainer>
   );
